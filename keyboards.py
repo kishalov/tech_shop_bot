ba59c9c0f30fd9a74edfea1a5a82f39c46957efb
Nikolai <kishalovn@gmail.com>
2025-10-13 08:36:12 +0400

    no help

--- keyboards.py
+++ keyboards.py
@@ -2,9 +2,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="📂 Меню")],
-        [KeyboardButton(text="🛒 Корзина"), KeyboardButton(text="ℹ️ Помощь")],
-    ],
-    resize_keyboard=True
+	keyboard=[
+		[KeyboardButton(text="📂 Меню")],
+		[KeyboardButton(text="🛒 Корзина")],
+	],
+	resize_keyboard=True
 )
