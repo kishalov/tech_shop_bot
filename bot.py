8af2ad8977c96366da705d7df67674337533caef
Nikolai <kishalovn@gmail.com>
2025-11-04 21:13:17 +0400

    no parser

--- bot.py
+++ bot.py
@@ -4,15 +4,21 @@ from config import BOT_TOKEN
 from handlers import router as base_router
 from handlers_catalog import router as catalog_router
 
+async def refresh_catalog_job():
+	while True:
+		from sheets import get_products
+		get_products(ttl=0)  # сбрасываем кэш
+		print("🔄 Каталог обновлён из таблицы.")
+		await asyncio.sleep(3600)  # раз в час
+
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+	bot = Bot(token=BOT_TOKEN)
+	dp = Dispatcher()
 
-    # подключаем роутеры
-    dp.include_router(base_router)
-    dp.include_router(catalog_router)
+	dp.include_router(base_router)
+	dp.include_router(catalog_router)
 
-    await dp.start_polling(bot)
+	# запускаем обновление каталога в фоне
+	asyncio.create_task(refresh_catalog_job())
 
-if __name__ == "__main__":
-    asyncio.run(main())
+	await dp.start_polling(bot)
